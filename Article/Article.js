16336c4017552089783d230e2828d29ab3a4774b
dpayton23 <darien.payton71@gmail.com>
2019-03-06 18:15:54 -0500

    updated expand-article method

--- Article/Article.js
+++ Article/Article.js
@@ -14,7 +14,8 @@ class Article {
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.expandButton.classList.toggle("toggle-on")
+    this.domElement.classList.toggle("article-open")
+    // console.log(this)
   }
 }
 
